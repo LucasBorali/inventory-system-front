cd595b3960badaaf3780a421c5762f78b40558aa
Lucas Borali <lucas.borali@gmail.com>
2024-11-07 08:35:39 -0300

    Criação do projeto

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import logo from './Logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Inventory System
+        </h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Login
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
